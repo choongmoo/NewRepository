1634b64e5595e6d5aaa53447437aa4b47b760a62
choongmoo <wdokl88@naver.com>
2024-10-16 18:19:57 +0900

    gwajae

---
+++ main1.py
@@ -0,0 +1,9 @@
+import max_profit as mp
+import prime_number as pn
+
+if __name__ == '__main__':
+    pn.is_prime_number(001)
+    pn.is_prime_number(103)
+    pn.is_prime_number(024)
+    pn.is_prime_number(031)
+    
